36e0253c568938a05733b6be499b7e801fcbe00b
SujanaMaroju <103292530+SujanaMaroju@users.noreply.github.com>
2024-01-20 20:43:42 -0600

    updated cart container

--- coffee.js
+++ coffee.js
@@ -26,4 +26,47 @@ window.onscroll=()=>{
     searchForm.classList.remove('active');
     cartItem.classList.remove('active');
 }
+//add to cart functionality
+let addtocartList=[];
+
+function addtocart(coffeeName,price,img){
+let addtocartobj={
+    coffee:coffeeName,
+    amount:price,
+    image:img
+}
+
+const isValExists = addtocartList.find((e) => e.coffee === coffeeName)
+if(!isValExists){
+  addtocartList.push(addtocartobj);
+    let Totalprice=document.querySelector(".total")
+    let totalcount=addtocartList.reduce((acc,e)=> acc+ (+e.amount),0)
+    Totalprice.innerText= `Total : ${totalcount}`;    
+
+    let pDivTag = document.createElement("div");
+   pDivTag.classList.add("cart-items");
+
+   let cSpanTag = document.createElement("span");
+   cSpanTag.classList.add('fas');
+   cSpanTag.classList.add('fa-times');
+   let cImgTag = document.createElement("img");
+   cImgTag.src= "images/"+addtocartobj.image;
+   let cDivTag = document.createElement("div");
+   cDivTag.classList.add("content");
+   let ccH3Tag = document.createElement("h3");
+    ccH3Tag.innerText = addtocartobj.coffee;
+   let ccDivTag = document.createElement("div");
+   ccDivTag.classList.add("price");
+   ccDivTag.innerText = addtocartobj.amount;
+
+   cDivTag.appendChild(ccH3Tag);
+   cDivTag.appendChild(ccDivTag);
+
+   pDivTag.appendChild(cSpanTag);
+   pDivTag.appendChild(cImgTag);
+   pDivTag.appendChild(cDivTag);
+
+   cartItem.appendChild(pDivTag);
+}   
+}
 
